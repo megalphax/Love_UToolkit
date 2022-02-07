99306759ec8b649fc0f386cab12f971bdf422856
megalphax <megalphax@gmaIL.com>
2022-02-07 22:45:55 +0100

    data 1.0

--- lua/UToolkit/data.lua
+++ lua/UToolkit/data.lua
@@ -5,6 +5,9 @@ function UTK.data.newDataStorage(pMode, pPath, pFilename)
 
         DS.lstData = {}
 
+        DS.keyword1 = " ::::> "
+        DS.keyword2 = " <:::: "
+
         DS.fullPath = ""
 
         if pMode == "GameFolder" then
@@ -18,22 +21,58 @@ function UTK.data.newDataStorage(pMode, pPath, pFilename)
         DS.file = io.open(DS.fullPath, "a")
         io.output(DS.file)
         DS.file:close()
-        DS.file = io.open(DS.fullPath, "r")
 
         function DS:loadFileData()
+            DS.file = io.open(DS.fullPath, "r")
             DS.fileContent = DS.file:read("*a")
+
+            local dataType = ""
+            local name = ""
+            local content = ""
+            local startKeyword1, endKeyword1, startKeyword2, endKeyword2, endOfLine = 0
+            
             for line in DS.fileContent:gmatch("([^\n]*)\n?") do
-                print(line)
+                if line ~= "" then
+                    startKeyword1, endKeyword1 = string.find(line, DS.keyword1)
+                    startKeyword2, endKeyword2 = string.find(line, DS.keyword2)
+                    endOfLine = string.len(line)
+
+                    print(string.find(line, DS.keyword1))
+                    print(line)
+
+                    print(string.sub(line, 0, startKeyword1-1))
+                    print(string.sub(line, endKeyword1+1, startKeyword2-1))
+                    print(string.sub(line, endKeyword2+1, endOfLine))
+
+                    DS:addData(string.sub(line, 0, startKeyword1-1), string.sub(line, endKeyword1+1, startKeyword2-1), string.sub(line, endKeyword2+1, endOfLine))
+                end
             end
+
             DS.file:close()
         end
 
         function DS:addData(pDataType, pName, pContent)
-            local data = {}
-            data.dataType = pDataType
-            data.name = pName
-            data.content = pContent
-            table.insert(DS.lstData, data)
+            local exist = false
+            for i=1, #DS.lstData do
+                if pName == DS.lstData[i].name then
+                    exist = true
+                end
+            end
+            if not exist then 
+                local data = {}
+                data.dataType = pDataType
+                data.name = pName
+                data.content = pContent
+                table.insert(DS.lstData, data)
+            end
+        end
+
+        function DS:setData(pName, pContent)
+            for i=1, #DS.lstData do
+                if DS.lstData[i].name == pName then 
+                    DS.lstData[i].content = pContent
+                end
+            end
         end
 
         function DS:getData(pName)
@@ -57,7 +96,7 @@ function UTK.data.newDataStorage(pMode, pPath, pFilename)
                 local name = DS.lstData[i].name
                 local content = DS.lstData[i].content
 
-                savedData = savedData .. (dataType.."::"..name.."->"..content..";"..'\n')
+                savedData = savedData .. (dataType..DS.keyword1..name..DS.keyword2..content..'\n')
 
             end
             
